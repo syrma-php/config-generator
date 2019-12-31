4cc4318bb23dc4e5d9c20ebc20e1e93d4d63614d
Tamas Szijarto <szijarto.tamas.developer@gmail.com>
2019-12-31 02:33:41 +0100

    Add tests for ParameterFileLoader

---
+++ tests/Config/Loader/YamlConfigFileLoaderTest.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Syrma\ConfigGenerator\Tests\Config\Loader;
+
+
+use Symfony\Component\Yaml\Parser;
+use Syrma\ConfigGenerator\Config\Loader\YamlParameterFileLoader;
+
+class YamlConfigFileLoaderTest extends AbstractParameterFileLoaderTest
+{
+
+    private function createLoader(): YamlParameterFileLoader
+    {
+        return new YamlParameterFileLoader(new Parser());
+    }
+
+    public function testEmpty(): void
+    {
+        $yml = $this->createFileRef(self::FILE_YML_EMPTY);
+        $yaml = $this->createFileRef(self::FILE_YAML_EMPTY);
+
+        $this->assertTrue($this->createLoader()->isSupported($yml));
+        $this->assertTrue($this->createLoader()->isSupported($yaml));
+
+        $this->assertEquals([], $this->createLoader()->load($yml));
+        $this->assertEquals([], $this->createLoader()->load($yaml));
+    }
+
+    public function testLoadDataFromRoot(): void
+    {
+        $yml = $this->createFileRef(self::FILE_YML_ROOT);
+        $this->assertEquals([
+            'foo' => 'bar',
+            'param' => [1,2,3]
+        ], $this->createLoader()->load($yml));
+    }
+
+    public function testLoadDataFromParameters(): void
+    {
+        $yml = $this->createFileRef(self::FILE_YML_PARAMS);
+        $this->assertEquals([
+            'bar' => 'foo',
+            'file' => 'param_parameters'
+        ], $this->createLoader()->load($yml));
+    }
+}
