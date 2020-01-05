0772cfec5a93cbfe2375aa72f70cad21556d14dd
Tamas Szijarto <szijarto.tamas.developer@gmail.com>
2020-01-05 01:30:53 +0100

    Clean

--- tests/Definition/Factory/fixtures/paramFile_0.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'paramFile_0' => 0,
-    'fooFile' => 'f0',
-];
--- tests/Definition/Factory/fixtures/paramFile_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'paramFile_1' => 1,
-    'fooFile' => 'f1',
-];
